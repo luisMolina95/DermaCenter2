92ad84a6135a98cefa758bc689cdbb97a7ae8f65
luisMolina95 <luisrodmolina@gmail.com>
2023-08-21 14:50:41 -0600

    Run formatter

--- app/database.py
+++ app/database.py
@@ -6,12 +6,14 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 engine = create_async_engine(os.environ.get('DATABASE_URL'), echo=True)
 SessionLocal = async_sessionmaker(engine)
 
+
 class Base(DeclarativeBase):
     pass
+
 
 async def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
-       await db.close()
+        await db.close()
--- app/models.py
+++ app/models.py
@@ -4,66 +4,87 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Enum, func, ForeignKey
 from app.database import Base
 
+
 class Sucursal(Base):
     __tablename__ = "sucursales"
     sucursal_id: Mapped[int] = mapped_column(primary_key=True)
     nombre: Mapped[str] = mapped_column(nullable=False)
     descripcion: Mapped[str] = mapped_column(nullable=False)
     regentes: Mapped[List["Regente"]] = relationship(back_populates="sucursal")
-    dependientes: Mapped[List["Dependiente"]] = relationship(back_populates="sucursal")
-    inventarios: Mapped[List["Inventario"]] = relationship(back_populates="sucursal")
+    dependientes: Mapped[List["Dependiente"]
+                         ] = relationship(back_populates="sucursal")
+    inventarios: Mapped[List["Inventario"]] = relationship(
+        back_populates="sucursal")
+
 
 class Regente(Base):
     __tablename__ = "regentes"
     regente_id: Mapped[int] = mapped_column(primary_key=True)
     nombre: Mapped[str] = mapped_column(nullable=False)
     jvpqf: Mapped[str] = mapped_column(nullable=False)
-    sucursal_id: Mapped[int] = mapped_column(ForeignKey("sucursales.sucursal_id"))
+    sucursal_id: Mapped[int] = mapped_column(
+        ForeignKey("sucursales.sucursal_id"))
     sucursal: Mapped["Sucursal"] = relationship(back_populates="regentes")
+
 
 class Dependiente(Base):
     __tablename__ = "dependientes"
     dependiente_id: Mapped[int] = mapped_column(primary_key=True)
     nombre: Mapped[str] = mapped_column(nullable=False)
     dui: Mapped[str] = mapped_column(nullable=False)
-    sucursal_id: Mapped[int] = mapped_column(ForeignKey("sucursales.sucursal_id"))
+    sucursal_id: Mapped[int] = mapped_column(
+        ForeignKey("sucursales.sucursal_id"))
     sucursal: Mapped["Sucursal"] = relationship(back_populates="dependientes")
+
 
 class Inventario(Base):
     __tablename__ = "inventarios"
     inventario_id: Mapped[int] = mapped_column(primary_key=True)
     nombre: Mapped[str] = mapped_column(nullable=False)
-    sucursal_id: Mapped[int] = mapped_column(ForeignKey("sucursales.sucursal_id"))
+    sucursal_id: Mapped[int] = mapped_column(
+        ForeignKey("sucursales.sucursal_id"))
     sucursal: Mapped["Sucursal"] = relationship(back_populates="inventarios")
     pedidos: Mapped[List["Pedido"]] = relationship(back_populates="inventario")
-    transferencias_origen: Mapped[List["Transferencia"]] = relationship(back_populates="inventario_origen", foreign_keys="Transferencia.inventario_origen_id")
-    transferencias_destino: Mapped[List["Transferencia"]] = relationship(back_populates="inventario_destino", foreign_keys="Transferencia.inventario_destino_id")
+    transferencias_origen: Mapped[List["Transferencia"]] = relationship(
+        back_populates="inventario_origen", foreign_keys="Transferencia.inventario_origen_id")
+    transferencias_destino: Mapped[List["Transferencia"]] = relationship(
+        back_populates="inventario_destino", foreign_keys="Transferencia.inventario_destino_id")
 
 
 class Pedido(Base):
     __tablename__ = "pedidos"
     pedido_id: Mapped[int] = mapped_column(primary_key=True)
     cantidad: Mapped[int] = mapped_column(nullable=False)
-    estado: Mapped[Literal["ingresado", "en proceso", "finalizado"]] = mapped_column(Enum("ingresado", "en proceso", "finalizado", name="estado_pedido"))
-    fecha: Mapped[datetime.datetime] = mapped_column(server_default=func.CURRENT_TIMESTAMP())
-    producto_id: Mapped[int] = mapped_column(ForeignKey("productos.producto_id"))
+    estado: Mapped[Literal["ingresado", "en proceso", "finalizado"]] = mapped_column(
+        Enum("ingresado", "en proceso", "finalizado", name="estado_pedido"))
+    fecha: Mapped[datetime.datetime] = mapped_column(
+        server_default=func.CURRENT_TIMESTAMP())
+    producto_id: Mapped[int] = mapped_column(
+        ForeignKey("productos.producto_id"))
     producto: Mapped["Producto"] = relationship(back_populates="pedidos")
-    inventario_id: Mapped[int] = mapped_column(ForeignKey("inventarios.inventario_id"))
+    inventario_id: Mapped[int] = mapped_column(
+        ForeignKey("inventarios.inventario_id"))
     inventario: Mapped["Inventario"] = relationship(back_populates="pedidos")
-    transferencias: Mapped[List["Transferencia"]] = relationship(back_populates="pedido")
-
+    transferencias: Mapped[List["Transferencia"]
+                           ] = relationship(back_populates="pedido")
 
 
 class Transferencia(Base):
     __tablename__ = "transferencias"
     transferencia_id: Mapped[int] = mapped_column(primary_key=True)
-    fecha: Mapped[datetime.datetime] = mapped_column(server_default=func.CURRENT_TIMESTAMP())
-    inventario_origen_id: Mapped[int] = mapped_column(ForeignKey("inventarios.inventario_id"))
-    inventario_origen: Mapped["Inventario"] = relationship(foreign_keys="Transferencia.inventario_origen_id", back_populates="transferencias_origen") 
-    inventario_destino_id: Mapped[int] = mapped_column(ForeignKey("inventarios.inventario_id"))
-    inventario_destino: Mapped["Inventario"] = relationship(foreign_keys="Transferencia.inventario_destino_id", back_populates="transferencias_destino") 
+    fecha: Mapped[datetime.datetime] = mapped_column(
+        server_default=func.CURRENT_TIMESTAMP())
+    inventario_origen_id: Mapped[int] = mapped_column(
+        ForeignKey("inventarios.inventario_id"))
+    inventario_origen: Mapped["Inventario"] = relationship(
+        foreign_keys="Transferencia.inventario_origen_id", back_populates="transferencias_origen")
+    inventario_destino_id: Mapped[int] = mapped_column(
+        ForeignKey("inventarios.inventario_id"))
+    inventario_destino: Mapped["Inventario"] = relationship(
+        foreign_keys="Transferencia.inventario_destino_id", back_populates="transferencias_destino")
     pedido_id: Mapped[int] = mapped_column(ForeignKey("pedidos.pedido_id"))
     pedido: Mapped["Pedido"] = relationship(back_populates="transferencias")
+
 
 class Producto(Base):
     __tablename__ = "productos"
